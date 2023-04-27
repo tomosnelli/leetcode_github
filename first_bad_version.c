376671e09c483929557bae549c888163416e380c
Tomo Kaneko <tkaneko@my.bcit.ca>
2023-04-27 14:12:16 -0700

    leetcode sesh with friend

---
+++ first_bad_version.c
@@ -0,0 +1,15 @@
+int firstBadVersion(int n) {
+  int low = 1;
+  int high = n;
+
+  while(low < high){
+    int middle = (low + high) / 2;
+    if(!isBadVersion(middle)){
+      low = middle + 1;
+    } else {
+      high = middle;
+    }
+  }
+
+  return low;
+}
