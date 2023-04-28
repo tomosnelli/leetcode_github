d86bd8288e0c129c62772a95ffb4aadc88bb1b41
Tomo Kaneko <tkaneko@my.bcit.ca>
2023-04-28 12:41:16 -0700

    do one more

---
+++ missing_number.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int sum_of_sequence(int n)
+{
+    int sum = 0;
+    for(int i = 0; i <= n; ++i){
+        sum += i;
+    }
+
+    return sum;
+}
+
+
+int missingNumber(int* nums, int numsSize)
+{
+    int sum = 0;
+    for(int i = 0; i < numsSize; ++i){
+        sum += nums[i];
+    }
+
+    sequence_sum = sum_of_sequence(numsSize);
+    return sequence_sum - sum;
+}
+
+int main()
+{
+    int nums[] = { 0, 1 };
+    int missing = missingNumber(nums, 2);
+}
