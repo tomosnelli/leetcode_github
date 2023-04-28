ac7540634ac0f1c77532237eab0e3a2f9fd4513f
Tomo Kaneko <tkaneko@my.bcit.ca>
2023-04-28 13:40:30 -0700

    modify style

--- missing_number.c
+++ missing_number.c
@@ -3,9 +3,7 @@
 int sum_of_sequence(int n)
 {
     int sum = 0;
-    for(int i = 0; i <= n; ++i){
-        sum += i;
-    }
+    for(int i = 0; i <= n; ++i) sum += i;
 
     return sum;
 }
@@ -14,9 +12,7 @@ int sum_of_sequence(int n)
 int missingNumber(int* nums, int numsSize)
 {
     int sum = 0;
-    for(int i = 0; i < numsSize; ++i){
-        sum += nums[i];
-    }
+    for(int i = 0; i < numsSize; ++i) sum += nums[i];
 
     sequence_sum = sum_of_sequence(numsSize);
     return sequence_sum - sum;
