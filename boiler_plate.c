4e9ce9dc614ad604089bbc98d54a4307288a3d3f
Tomo Kaneko <tkaneko@my.bcit.ca>
2023-05-06 23:01:58 -0700

    Add implementation

---
+++ boiler_plate.c
@@ -0,0 +1,23 @@
+/*
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+int main()
+{
+    return 0;
+}
