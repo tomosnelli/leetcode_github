4832bb6d7e495a67c951d35536bc735b2b26b7bc
Tomo Kaneko <tomo.kaneko@proton.me>
2023-05-20 11:10:34 -0700

    Add empty file

---
+++ combination_sum_2.c
@@ -0,0 +1,9 @@
+/*
+    40. Combination Sum 2
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int** combinationSum2(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
+
+}
