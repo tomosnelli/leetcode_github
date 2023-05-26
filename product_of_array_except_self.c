8021b142fb6aaae399c77dffc510ba07f5839af7
Tomo Kaneko <tomo.kaneko@proton.me>
2023-05-25 17:48:37 -0700

    Add implementation

---
+++ product_of_array_except_self.c
@@ -0,0 +1,21 @@
+int* productExceptSelf(int* nums, int numsSize, int* returnSize)
+{
+    int* result = malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+
+    int product = 1;
+
+    for(int i = 0; i < numsSize; ++i){
+        result[i] = product;
+        product *= nums[i];
+    }
+
+    product = 1;
+
+    for(int i = numsSize -1; i >=0; --i){
+        result[i] *= product;
+        product *= nums[i];
+    }
+
+    return result;
+}
