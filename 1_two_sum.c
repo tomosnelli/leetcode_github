df524f46d9c84b4d5219642e2683c59e91ce6fb9
Tomo Kaneko <tomo.kaneko@proton.me>
2023-06-01 21:31:47 -0700

    Add hash table implementation

--- 1_two_sum.c
+++ 1_two_sum.c
@@ -5,10 +5,13 @@
 #include "uthash.h"
 
 typedef struct{
-    int val;
+    int key;
+    int index;
     UT_hash_handle hh;
 } HashTable;
 
+/*
+// O(n^2) solution
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     int* return_array = malloc(2 * sizeof(int));
@@ -23,4 +26,34 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
         }
     }
     return return_array;
+}
+*/
+
+// hash table
+int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    HashTable* hash_table = NULL;
+    HashTable* entry = NULL;
+
+    int* result = malloc(2 * sizeof(int));
+    *returnSize = 2;
+
+    for(int i = 0; i < numsSize; ++i){
+        int complement = target - nums[i];
+        HASH_FIND_INT(hash_table, &complement, entry);
+
+        if(entry){
+            result[0] = entry->index;
+            result[1] = i;
+            break;
+        }
+
+        entry = malloc(sizeof(HashTable));
+        entry->key = nums[i];
+        entry->index = i;
+        HASH_ADD_INT(hash_table, key, entry);
+    }
+
+    HASH_CLEAR(hh, hash_table);
+    return result;
 }
