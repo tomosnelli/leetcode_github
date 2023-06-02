6bf1e4980479aa14c1cef382e1f6ab3bb3fe0908
Tomo Kaneko <tomo.kaneko@proton.me>
2023-06-01 19:45:16 -0700

    Add useful code

---
+++ deep_copy_linked_list.c
@@ -0,0 +1,47 @@
+/*
+    memo for how to make a deep copy of a linked list
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+    struct ListNode *random;
+};
+
+struct ListNode* create_node(int val)
+{
+    struct ListNode* node = malloc(sizeof(struct ListNode));
+    if(!node){
+        perror("Memory Allocation Failed.\n");
+        exit(1);
+    }
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+
+void insert_node(struct ListNode** head, int val)
+{
+    struct ListNode* node = create_node(val);
+    if(*head == NULL){
+        *head = node;
+    } else {
+        struct ListNode* current = *head;
+        while(current->next){
+            current = current->next;
+        }
+        current->next = node;
+    }
+}
+
+struct Node* copyRandomList(struct Node* head) {
+    if(!head) return NULL;
+
+    struct ListNode* node = create_node(head->data);
+
+    node->next = copyRandomList(head->next);
+
+    return node;
+}
