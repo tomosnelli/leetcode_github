7f868ae05c5e4352386f8ae8d4bacd05aba9c792
Tomo Kaneko <tomo.kaneko@proton.me>
2023-06-07 10:23:20 -0700

    Add file to work on later

---
+++ solutions/235_lowest_common_ancestor_of_a_binary_search_tree.c
@@ -0,0 +1,13 @@
+/*
+    235. Lowest Common Ancestor of a Binary Search Tree
+*/
+#include "../utils/common.h"
+
+struct TreeNode* lowestCommonAncestor(
+    struct TreeNode* root,
+    struct TreeNode* p,
+    struct TreeNode* q
+)
+{
+    
+}
