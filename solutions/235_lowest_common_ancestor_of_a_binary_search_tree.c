24667131f0f2e1cbd2fe44ab58b58eedb9eb6b39
Tomo Kaneko <tomo.kaneko@proton.me>
2023-06-07 18:48:19 -0700

    Add implementation

--- solutions/235_lowest_common_ancestor_of_a_binary_search_tree.c
+++ solutions/235_lowest_common_ancestor_of_a_binary_search_tree.c
@@ -9,5 +9,30 @@ struct TreeNode* lowestCommonAncestor(
     struct TreeNode* q
 )
 {
-    
+    if(!root) return NULL;
+
+    if(p->val < root->val && q->val < root->val){
+        return lowestCommonAncestor(root->left, p, q);
+    }
+
+    if(p->val > root->val && q->val > root->val){
+        return lowestCommonAncestor(root->right, p, q);
+    }
+
+    if((p->val < root->val && q->val > root->val)
+            ||
+        (p->val > root->val && q->val < root->val) 
+    ){
+        return root;
+    }
+
+    if(p->val == root->val){
+        return p;
+    }
+
+    if(q->val == root->val){
+        return q;
+    }
+
+    return root;
 }
