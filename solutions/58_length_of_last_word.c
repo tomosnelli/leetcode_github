2f57cd00034601fbb4b8aeac3832d7fc366f1580
Tomo Kaneko <tomo.kaneko@proton.me>
2023-06-17 16:33:10 -0700

    Add implementation

---
+++ solutions/58_length_of_last_word.c
@@ -0,0 +1,43 @@
+/*
+    58. Length of Last Word
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SPACE ' '
+#define END '\0'
+
+char* moveToWord(char* s){
+    while(*s == SPACE && *s != END){
+        ++s;
+    }
+
+    return s;
+}
+
+int countWord(char* s){
+    if(*s == END) return 0;
+
+    int count = 0;
+
+    while(*s != END && *s != SPACE){
+        ++count;
+        ++s;
+    }
+
+    return count;
+}
+
+int lengthOfLastWord(char* s){
+    int count = 0;
+
+    while(*s != END){
+        s = moveToWord(s);
+        int temp = countWord(s);
+        count = temp > 0 ? temp : count;
+        // tomo homo
+        s += temp;
+    }
+
+    return count;
+}
